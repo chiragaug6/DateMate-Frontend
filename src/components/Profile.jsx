c42c883c415816532776ba9086471b558cfb57a1
Chirag-Solanki6804 <crs279836@gmail.com>
2025-04-26 22:34:00 +0530

    major frontend features done

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux";
+import EditProfile from "./EditProfile";
+import { useEffect } from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <EditProfile />
+    </div>
+  );
+};
+export default Profile;
