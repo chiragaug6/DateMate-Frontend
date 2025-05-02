50e8cada7b58a82185c6579a933e0c81882bbede
Chirag-Solanki6804 <crs279836@gmail.com>
2025-05-02 11:30:50 +0530

    deployment url config changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,4 @@
 export const BASE_URL =
   location.hostname === "localhost"
-    ? "http://localhost:3000/api/v1"
-    : "http://13.60.91.56/api/v1";
-
-export const FRONTEND_URL = "http://localhost:3000";
+    ? "http://localhost:3000"
+    : "https://codemate.tech/api";
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,11 +1,9 @@
 import io from "socket.io-client";
-import { BASE_URL, FRONTEND_URL } from "./constants";
+import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
-    return io(FRONTEND_URL, {
-      transports: ["websocket"],
-    });
+    return io(BASE_URL);
   } else {
     return io("/", { path: "/api/socket.io" });
   }
