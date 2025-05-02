27664b125a3bf5d374040c6f7b9ced366b44231b
Chirag-Solanki6804 <crs279836@gmail.com>
2025-05-02 13:25:02 +0530

    base url change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,2 @@
 export const BASE_URL =
-  location.hostname === "localhost"
-    ? "http://localhost:3000"
-    : "https://codemate.tech/api";
+  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
